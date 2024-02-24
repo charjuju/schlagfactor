ab5863f22b18ab216195797e15ffad9a0f9b9ca4
truchejude <jude.marzat@epitech.eu>
2024-02-24 19:43:22 +0100

    [UP] decoration

--- src/App.js
+++ src/App.js
@@ -130,12 +130,12 @@ function App() {
   };
 
   return (
-    <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: '#28a745' }}>
         <h2>Le Schlag Factor est le meilleur moyen de calculer la qualité cuite/prix.</h2>
         <h2> Plus il est élevé, meilleure sera ta cuite pour ton portefeuille.</h2>
       </div>
-      <form style={{ marginTop: '100px', margin: '25px'}} onSubmit={calculateSchlagFactor}>
+      <form style={{ marginTop: '100px', margin: '25px' }} onSubmit={calculateSchlagFactor}>
         <div>
           <label>
             {"Image Url (obtionel)"}:
@@ -194,7 +194,7 @@ function App() {
             <h2>Schlag Factor de pour {nom} {schlagFactor}:</h2>
           </div>
         )}
-        <button style={{marginBottom: '25px'}} onClick={downloadJson}>Télécharger dataBeerShlag.json</button>
+        <button style={{ marginBottom: '25px' }} onClick={downloadJson}>Télécharger dataBeerShlag.json</button>
         <input
           type="file"
           accept=".json"
@@ -205,16 +205,20 @@ function App() {
             <div key={index} >
 
               <div className='stateConteneur' key={index} style={{}}>
-                {data.imageUrl === '' ?
-                  <div style={{ height: '64px', width: '64px', backgroundColor: 'rgba(134, 134, 134, 0.28)', borderRadius: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <p>NO</p>
-                  </div>
-                  :
-                  <img alt='beerProfil' src={data.imageUrl} style={{ height: '64px', width: '64px' }}></img>
-                }
-                <h1><strong>{index + 1}</strong></h1>
-                <h1><strong>{data.nom}</strong></h1>
-                <p>{data.cl}CL - {data.tauxAlcool}% - {data.prix}€ = <strong>SCORE: {data.schlagFactor}</strong></p>
+                <div className='img-and-top'>
+                  {data.imageUrl === '' ?
+                    <div style={{ height: '64px', width: '64px', backgroundColor: 'rgba(134, 134, 134, 0.28)', borderRadius: '100px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                      <p>NO</p>
+                    </div>
+                    :
+                    <img alt='beerProfil' src={data.imageUrl} style={{ height: '64px', width: '64px' }}></img>
+                  }
+                  <h1><strong>{index + 1}</strong></h1>
+                </div>
+                <div className='nom-and-score'>
+                  <h1><strong>{data.nom}</strong></h1>
+                  <p>{data.cl}CL - {data.tauxAlcool}% - {data.prix}€ = <strong>SCORE: {data.schlagFactor}</strong></p>
+                </div>
                 {editIndex === index ? (
                   <div style={{ position: 'fixed', backgroundColor: 'rgba(40, 167, 69, 0.28)', width: '100%', height: '100vh', top: 0, left: 0, justifyContent: 'center', alignItems: 'center', display: 'flex' }}>
                     <div style={{ width: '50vw', backgroundColor: 'white', borderRadius: '24px', display: 'flex', flexWrap: 'wrap', padding: '50px' }}>
@@ -270,7 +274,7 @@ function App() {
                     </div>
                   </div>
                 ) : (
-                  <button style={{width: '70px'}} onClick={() => handleEditClick(index)}>Edit</button>
+                  <button style={{ width: '70px' }} onClick={() => handleEditClick(index)}>Edit</button>
                 )}
               </div>
               <div style={{ borderBottom: '1px solid black' }} />
